237acd38546ac0b72f5a73000641200c32abd997
“Marc <marcaugustblumberg@gmail.com>
2023-08-23 19:41:26 -0600

    Updated home page and bio page

--- pages/bio.jsx
+++ pages/bio.jsx
@@ -6,16 +6,26 @@ import Image from 'next/image'
 export default function Bio() {
     return (
         <Layout>
-            <p>My research focuses on the role of stress in shaping child development, including its role in critical outcomes in their lives such as their health and academic achievement. I focus on the moderators of these processes, such as individual differences in stress neurobiology and cognitive processes like executive function. I have used and will continue to employ measures spanning multiple levels of analysis (e.g., endocrine and immune biomarkers, EKG data, survey data, behavioral measures) to (1) understand socioeconomic disparities in health and academic achievement and underlying mechanisms; (2) examine the role of stress in shaping regulatory abilities like executive function; (3) uncover protective factors for children undergoing stress.</p>
-            <p>I am a graduate student affiliate of the <a target="_blank" href="https://poverty.ucdavis.edu/">UC Davis Center for Poverty Research</a>, you can read my recent policy brief <a target="_blank" href="https://poverty.ucdavis.edu/post/boosting-executive-function-may-help-close-income-based-achievement-gap">here</a>. I am also a mentor in the <a target="_blank" href="https://urc.ucdavis.edu/">UC Davis Undergraduate Research Center</a>'s <a target="_blank" href="https://urc.ucdavis.edu/MMP">Mentor-Mentee Program in Humanities, Arts, Cultural Studies and Social Sciences</a>.</p>
-            <p>You can download my latest CV <a href="/pdfs/LillyBelleDeerCV 5-18-21.pdf" target="_blank">here</a>.</p>
+            <p>
+                I was born and raised in Honolulu, Hawaii. I then attended Claremont McKenna College, where I majored in Psychology and was an NCAA athlete competing in swimming and water polo. I joined the <a target="_blank" href="https://psychologylawpolicylab.com/people">Psychology, Law, and Policy Lab</a> in my junior year to work with Dr. Daniel Krauss, where I conducted research on jury decision making. I also completed my senior honors thesis in the lab, where I studied how jurors assess the credibility of expert testimony of therapists in sexual assault trials. In my junior and senior years I also joined the <a target="_blank" href="https://www.cmc.edu/autism-center">Claremont Autism Center</a> as a research assistant, where I worked with Dr. Lindsey Sterling on a research study assessing romantic relationships among young adults with autism. I spent my senior year working as a research assistant in the <a target="_blank" href="https://www.bergerinstitute.org/">Berger Institute for Work, Family and Children</a> with Dr. Tomoe Kanaya. During that year and the year after I graduated, I conducted research on barriers such as anxiety for college students and young adults entering the workforce. 
+            </p>
+            <p>
+                To gain training in the biological embedding of early experiences across development, I pursued my PhD in Developmental Psychology at the University of California, Davis with Dr. Camelia Hostinar. In the <a target="_blank" href="https://seslab.ucdavis.edu/">Social Environment and Stress Lab</a>, my colleagues and I conducted studies of the effects of early stress (e.g., low socioeconomic status, acute stress) on children's health and development. My dissertation focused on how different types of stress (low socioeconomic stress, acute stress, and Covid-19-related financial stress) impacts the executive function performance of children and adolescence. 
+            </p>
+            <p>
+                During my graduate training, I often had questions about how the prenatal and postnatal environments might interact to influence child outcomes such as physical and mental health. These questions led me to pursue further training in the assessment of prenatal influences on child health. I pursued my postdoctoral training with Drs. Elysia Davis, Jenalee Doom, and Benjamin Hankin at the <a target="_blank" href="https://careprojectdenver.org/current-projects">Care Project</a> at the University of Denver. I am pursuing this training with funding from the NHLBI (<a target="_blank" href="https://reporter.nih.gov/search/ZI0lXL5oNUO6aaWMMdRKMg/project-details/10538061">F32HL165844</a>) on the association between placental corticotrophin releasing hormone and both child cardiovascular disease risk and obesogenic eating behaviors. I also am currently conducting research on prenatal and infancy experiences on child stress neurobiology, health, and behavior.
+            </p>
+            <p>
+                My aim for my future research is to examine interactions between the prenatal and postnatal environment in predicting child physical and mental health, and to have a focus on early targets for intervention. 
+            </p>
+            <p>You can download my latest CV <a href="pdfs/LillyBelleDeerCV 8.23.23.pdf" target="_blank">here</a>.</p>
             <Container>
                 <Row>
                     <Col md={4} className={styles.bioImageCol}>
-                        <Image className={styles.bioImage} src="/images/UC Psych_for About Page.jpg" height={260} width={260} priority='true' alt="LillyBelle Deer - UC Davis Department of Psychology" />
+                        <Image className={styles.bioImage} src="/images/IMG_5572.jpeg" height={260} width={260} priority='true' alt="LillyBelle Deer - NLBI" />
                     </Col>
                     <Col md={4} className={styles.bioImageCol}>
-                        <Image className={styles.bioImage} src="/images/CPR_for About Page.png" height={125} width={260} priority='true' alt="LillyBelle Deer - Center for Poverty Research" />
+                        <Image className={styles.bioImage} src="/images/IMG_7043.png" height={125} width={260} priority='true' alt="LillyBelle Deer - Care Project" />
                     </Col>
                     <Col md={4} className={styles.bioImageCol}>
                         <Image className={styles.bioImage} src="/images/SES Lab_for About Page.png" height={235} width={260} priority='true' alt="LillyBelle Deer - Social Environment and Stress Lab" />
--- pages/index.jsx
+++ pages/index.jsx
@@ -13,8 +13,12 @@ export default function Home() {
           </Col>
           <Col md={6}>
             <div className={styles.homeIntro}>
-              <p>Hello! I am a Developmental Psychology Ph.D. Candidate at the University of California, Davis. I study the role of stress in shaping child development, including outcomes such as health and academic achievement. I am particularly interested in moderators of these processes, such as individual differences in stress neurobiology and cognitive processes like executive function.</p>
-              <p>I am completing my doctoral studies with <a target="_blank" href="https://psychology.ucdavis.edu/people/hostinar">Camelia E. Hostinar</a> in the <a target="_blank" href="http://seslab.ucdavis.edu/">Social Environment and Stress Lab</a>. I received my B.A. from Claremont McKenna College in 2015, my M.A. from UC Davis in 2018, and will complete my Ph.D. from UC Davis in Summer 2021.</p>
+              <p>
+                Hello! My name is LillyBelle Deer and I am a F32 postdoctoral researcher at the University of Denver. My research integrates developmental and health psychology to understand how stress and early experience “gets under the skin” and impacts physical and mental health. I currently work with Drs. Elysia Davis, Jenalee Doom, and Benjamin Hankin at the <a target="_blank" href="https://careprojectdenver.org/current-projects">Care Project</a> at the University of Denver. I am currently funded by the National Heart Lung and Blood Institute on a F32 grant where I aim to assess the role of placental corticotrophin releasing hormone (CRH) in the development of child cardiovascular health and eating behaviors. I am particularly interested in prenatal and early childhood experiences that shape the health and behavior of children.
+              </p>
+              <p>
+                I completed my doctoral studies in Developmental Psychology with <a target="_blank" href="http://seslab.ucdavis.edu/">Dr. Camelia E. Hostinar</a> at the University of California Davis in June 2021, where I studied the effects of acute and chronic stress on children's stress neurobiology, health, and cognitive processes such as executive function. I received my B.A. from Claremont McKenna College in 2015.
+              </p>
             </div>
           </Col>
         </Row>
